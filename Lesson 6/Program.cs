f7642a54b8b85ee5179dd2d2d226e179c8a51941
Ольга Крень <olga1k@yandex.ru>
2022-12-05 01:21:45 +0300

    Lesson 6 done

--- Lesson 6/Program.cs
+++ Lesson 6/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
+Console.Write("Input elements separated by spaces: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Quantity of elements > 0 is: {count}");
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+/*
+Console.WriteLine("Input value of b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input value of k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input value of b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input value of k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Intersection is at a point: ({x};{y})");
+*/
+
