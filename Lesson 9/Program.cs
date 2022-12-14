761a81d78edcac2a6fbbadc9c4d0ecdc1b08f43d
Ольга Крень <olga1k@yandex.ru>
2022-12-14 14:13:48 +0300

    Task 64

--- Lesson 9/Program.cs
+++ Lesson 9/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+/*
+void ShowNumbers (int n)
+{
+    if(n >= 1)
+    {
+        Console.Write(n + " ");
+        if(n > 1) ShowNumbers(n - 1);
+    }
+}
+ShowNumbers(5);
+*/
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
